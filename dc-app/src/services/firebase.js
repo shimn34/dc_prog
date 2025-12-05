f903f438fc71bab2a8c5405c09265636a2c9a662
shimn34 <abeshimpei34@gmail.com>
2025-12-05 16:50:10 +0900

    .env

--- dc-app/src/services/firebase.js
+++ dc-app/src/services/firebase.js
@@ -1,24 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCYBcWXJ9v9nn1hlbHIOsW0HaJRNCDbM80",
-  authDomain: "dc2025-project-a510e.firebaseapp.com",
-  projectId: "dc2025-project-a510e",
-  storageBucket: "dc2025-project-a510e.firebasestorage.app",
-  messagingSenderId: "854171673082",
-  appId: "1:854171673082:web:7a97c1ad158f205b12c443"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// 機能を export（他のファイルで使えるようにする）
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
